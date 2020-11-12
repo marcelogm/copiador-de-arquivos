79ed093d16afb33429b5fd2be0a193ddd1c462c3
Marcelo Gomes Martins <marcelo-martins@defensoria.rs.def.br>
2020-11-11 22:44:50 -0300

    fix: uses File.separator

--- src/main/java/br/com/marcelogm/sfcopier/customizer/dir/YearAndMonthByModifiedTime.java
+++ src/main/java/br/com/marcelogm/sfcopier/customizer/dir/YearAndMonthByModifiedTime.java
@@ -1,5 +1,6 @@
 package br.com.marcelogm.sfcopier.customizer.dir;
 
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
@@ -13,7 +14,7 @@ public class YearAndMonthByModifiedTime implements DirectoryBuilder {
         try {
             BasicFileAttributes attr = Files.readAttributes(path, BasicFileAttributes.class);
             LocalDateTime date = LocalDateTime.ofInstant(attr.lastModifiedTime().toInstant(), ZoneId.systemDefault());
-            return date.getYear() + "\\" + date.getMonth().getValue() + " - " + date.getMonth().toString().toLowerCase() + "\\";
+            return date.getYear() + File.separator + date.getMonth().getValue() + " - " + date.getMonth().toString().toLowerCase() + File.separator;
         } catch (Exception e) {
             return "";
         }
